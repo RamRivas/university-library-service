3b8b27aea1f9aafbd29b757c47d91e004199c421
Benjamin Ramirez <rambenrial@gmail.com>
2023-05-03 15:30:07 -0600

    Created roles and user models

---
+++ src/models/role.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const roleSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        enum: [ 'Student', 'Librarian' ]
+    }
+});
+
+const Role = mongoose.model('Role', roleSchema);
+
+const createRole = async ({name}) => {
+    try {
+        const role = new Role(
+            {
+                name
+            }
+        );
+    
+        const result = await role.save();
+        return result;
+    } catch (error) {
+        throw new Error(`Error ${error.message}. Path ${__filename}`);
+    }
+};
+
+module.exports = {
+    Role,
+    roleSchema,
+    createRole
+};
---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {SALT_ROUNDS} = require('../config');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    pwd: {
+        type: String,
+        required: true
+    },
+})
+
+const User = mongoose.model('User', userSchema);
+
+const createUser = async ({name, pwd, role}) => {
+    try {
+        const user = new User(
+            {
+                name,
+                pwd: await bcrypt.hash(pwd, parseInt(SALT_ROUNDS)),
+                role
+            }
+        );
+    
+        const result = await user.save();
+        return result;
+    } catch (error) {
+        throw new Error(`Error ${error.message}. Path ${__dirname}${__filename}`);
+    }
+}
+
+module.exports = {
+    User,
+    userSchema,
+    createUser
+};
