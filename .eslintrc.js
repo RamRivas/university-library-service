05d9eb1d466cfdb681847fa9ac1c6f4b69806f28
Benjamin Ramirez <rambenrial@gmail.com>
2023-05-03 15:24:16 -0600

    Setup Commit (Configurations and project init)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 'tab'],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
+};
