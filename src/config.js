6f351bdc83a76f7bbaed15269ff3cf48d59c0a75
Benjamin Ramirez <rambenrial@gmail.com>
2023-05-03 15:25:07 -0600

    Loading env variables with dotenv

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+const assert = require('assert');
+
+dotenv.config();
+
+const { PORT, CTX, SALT_ROUNDS, URI } = process.env;
+
+const config = {
+    PORT,
+    CTX,
+    SALT_ROUNDS,
+    URI
+};
+
+for (const element in config)
+    assert(config[element], `${element} missing on .env file`);
+
+module.exports = config;
