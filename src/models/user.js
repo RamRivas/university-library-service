973ae6faa619c4886510cf921c35a4d12553c28e
Benjamin Ramirez <rambenrial@gmail.com>
2023-05-04 08:38:08 -0600

    Created Login route

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const {SALT_ROUNDS} = require('../config');
+const { SALT_ROUNDS } = require('../config');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -9,31 +9,52 @@ const userSchema = mongoose.Schema({
     },
     pwd: {
         type: String,
-        required: true
+        required: true,
     },
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
-const createUser = async ({name, pwd, role}) => {
+const createUser = async ({ name, pwd, role }) => {
     try {
-        const user = new User(
-            {
-                name,
-                pwd: await bcrypt.hash(pwd, parseInt(SALT_ROUNDS)),
-                role
-            }
-        );
-    
+        const user = new User({
+            name,
+            pwd: await bcrypt.hash(pwd, parseInt(SALT_ROUNDS)),
+            role,
+        });
+
         const result = await user.save();
         return result;
     } catch (error) {
-        throw new Error(`Error ${error.message}. Path ${__dirname}${__filename}`);
+        throw new Error(
+            `Error ${error.message}. Path ${__dirname}${__filename}`
+        );
     }
-}
+};
+
+const login = async ({ name, pwd }) => {
+    try {
+        const user = await User.findOne({ name }).exec();
+        if (!user) {
+            return 'User not found';
+        }
+
+        const correctPwd = await bcrypt.compare(pwd, user.pwd);
+        if(!correctPwd) {
+            return 'Incorrect Password';
+        };
+
+        return 'You are now logged in';
+    } catch (error) {
+        throw new Error(
+            `Error ${error.message}. Path ${__dirname}${__filename}`
+        );
+    }
+};
 
 module.exports = {
     User,
     userSchema,
-    createUser
+    createUser,
+    login
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { user: { createUser }, role: { Role } } = require('../models');
+const { user: { createUser, login }, role: { Role } } = require('../models');
 const router = express.Router();
 
 // Create user
@@ -7,6 +7,16 @@ router.post("/users", async (req, res) => {
     try {
         req.body.role = Role.find({name: req.body.role});
         const result = await createUser(req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).send(`${error.message}`);
+    }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+    try {
+        const result = await login(req.body);
         res.status(200).json(result);
     } catch (error) {
         res.status(400).send(`${error.message}`);
