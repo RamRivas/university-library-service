9269c135a8b45cb8a57c11f60d01f3f414304333
Benjamin Ramirez <rambenrial@gmail.com>
2023-05-09 21:21:43 -0600

    getRoles controller

--- src/controllers/role.js
+++ src/controllers/role.js
@@ -1,4 +1,4 @@
-const { createRole } = require('../models/role');
+const { createRole, getAllRoles } = require('../models/role');
 
 // Create Role
 exports.postRoles = async (req, res) => {
@@ -9,3 +9,12 @@ exports.postRoles = async (req, res) => {
         res.status(400).send(`${error.message}`);
     }
 };
+
+exports.getRoles = async (req, res) => {
+    try {
+        const result = await getAllRoles();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).send(`${error.message}`);
+    }
+}
--- src/models/role.js
+++ src/models/role.js
@@ -37,4 +37,5 @@ module.exports = {
     Role,
     roleSchema,
     createRole,
+    getAllRoles
 };
